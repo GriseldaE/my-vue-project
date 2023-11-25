3e95618b722a5f7577842fbef06c84fd0e60f539
Julian F. Fuentes T <56417636+SaGiT2711@users.noreply.github.com>
2023-11-25 15:43:11 -0700

    componentes: Chat y busqueda
    creacion de componentes para chat y busqueda (se necesita revision para ajustes)

---
+++ src/components/Busqueda.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+      <input v-model="searchQuery" @input="search" placeholder="Buscar usuarios...">
+      <ul>
+        <li v-for="user in filteredUsers" :key="user.id">{{ user.name }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <!-- datos reales -->
+  <!-- <script>
+  export default {
+    data() {
+      return {
+        searchQuery: '',
+        users: [
+          { id: 1, name: 'Usuario 1' },
+          { id: 2, name: 'Usuario 2' },
+          { id: 3, name: 'Usuario 3' },
+          // ... más usuarios
+        ]
+      };
+    },
+    computed: {
+      filteredUsers() {
+        return this.users.filter(user =>
+          user.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+        );
+      }
+    },
+    methods: {
+      search() {
+        // Puedes realizar acciones adicionales al escribir en la barra de búsqueda si es necesario
+        console.log('Buscando...', this.searchQuery);
+      }
+    }
+  };
+  </script> -->
+
+  <!-- dummy data -->
+  <script>
+export default {
+  data() {
+    return {
+      searchQuery: '',
+      users: [
+        { id: 1, name: 'Usuario 1', email: 'usuario1@example.com' },
+        { id: 2, name: 'Usuario 2', email: 'usuario2@example.com' },
+        { id: 3, name: 'Usuario 3', email: 'usuario3@example.com' },
+        // ... más usuarios
+      ]
+    };
+  },
+  computed: {
+    filteredUsers() {
+      return this.users.filter(user =>
+        user.name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        user.email.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    }
+  },
+  methods: {
+    search() {
+      // Puedes realizar acciones adicionales al escribir en la barra de búsqueda si es necesario
+      console.log('Buscando...', this.searchQuery);
+    }
+  }
+};
+</script>
+
+  
+  <style scoped>
+  /* Estilos opcionales para dar formato a la lista de usuarios, etc. */
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    margin: 5px 0;
+    padding: 5px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+  }
+  </style>
+  
---
+++ src/components/Chat.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+      <div v-for="message in messages" :key="message.id">
+        <p :class="{ 'sent-message': message.sent, 'received-message': !message.sent }">
+          {{ message.text }}
+        </p>
+      </div>
+      <input v-model="newMessage" @keyup.enter="sendMessage" placeholder="Escribe un mensaje..." />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        messages: [
+          { id: 1, text: 'Hola', sent: true },
+          { id: 2, text: '¡Hola! ¿Cómo estás?', sent: false }
+        ],
+        newMessage: ''
+      };
+    },
+    methods: {
+      sendMessage() {
+        if (this.newMessage.trim() !== '') {
+          this.messages.push({ id: this.messages.length + 1, text: this.newMessage, sent: true });
+          this.newMessage = '';
+          // Aquí podrías agregar la lógica para enviar el mensaje a través de una red social.
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .sent-message {
+    background-color: #4CAF50;
+    color: white;
+    padding: 10px;
+    border-radius: 5px;
+    margin: 5px 0;
+  }
+  
+  .received-message {
+    background-color: #ddd;
+    padding: 10px;
+    border-radius: 5px;
+    margin: 5px 0;
+  }
+  </style>
+  
